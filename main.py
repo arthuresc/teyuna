0ff558163ec513f61726ce21c5724287a7cede2e
Arthur Escalera <arthurescalera@gmail.com>
2025-11-19 16:12:23 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/produto")
+async def read_produto():
+    return {"body": {"name": "Chapéu", "subTitle": "Baita chapéu bonito, sô"}}
